328c265b7013210ade627cc3afba3e66e1f2e1da
sajal <sajalcse2018@gmail.com>
2023-09-30 16:17:01 +0600

    Add project files.

---
+++ CSharp-arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿//C# Arrays
+//One Dimensional Arrays
+
+
+int[] intArray = new int[10];
+intArray[0] = 10;
+intArray[1] = 20;
+intArray[2] = 30;
+intArray[3] = 40;
+intArray[4] = 50;
+intArray[5] = 60;
+intArray[6] = 70;
+intArray[7] = 80;
+intArray[8] = 90;
+intArray[9] = 100;
+
+Console.WriteLine("For Loop: ");
+for(int i = 0; i <= intArray.Length; i++)
+{
+    Console.WriteLine(intArray[i]);
+}
+
+
